d3ae889ee5fa2a677bf1190a975c2873714fa8a1
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2019-04-15 08:23:48 -0700

    breakdowns list rendering

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 
 const List = props => {
-  const listItems = props.items.map(d => <li key={d.added}>{d.added}</li>);
+  const listItems = props.items.map(d => (
+    <li key={d.added}>
+      added: {d.added}
+      <br />
+      title: {d.title}
+      <br />
+      type: {d.type}
+      <br />
+      casting director: {d.castingDirector}
+      <br />
+      start date: {d.startDate}
+      <br />
+      I'm interested
+      <input type="checkbox" />
+    </li>
+  ));
 
-  return <ul>{listItems}</ul>;
+  return (
+    <ul>
+      {listItems}
+      <br />
+    </ul>
+  );
 };
 
 export default List;
